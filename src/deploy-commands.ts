2dfb0ef5918f9c8514d85fe783d06ad0114fefa1
doinkythederp <doinkythederp@icloud.com>
2023-02-09 21:12:10 +0000

    make deploy commands work again

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,34 +1,32 @@
-import { REST, Routes } from 'discord.js';
-import { clientId, guildId, token } from './config.json';
-import fs from 'node:fs';
+import { REST, Routes } from "discord.js";
+import { clientId, guildId, token } from "./config.json";
+import fs from "node:fs";
+import Command from "./command";
 
 const commands = [];
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+    const command = require(`./commands/${file}`) as Command;
+    commands.push(command.data);
 }
 
-
-const rest = new REST({ version: '10' }).setToken(token);
-
+const rest = new REST({ version: "10" }).setToken(token);
 
 (async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    console.log(
+        `Started refreshing ${commands.length} application (/) commands.`
+    );
 
+    const data = await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId),
+        { body: commands }
+    );
 
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	}
-    catch (error) {
-
-		console.error(error);
-	}
-})();
+    console.log(
+        `Successfully reloaded ${data.length} application (/) commands.`
+    );
+})().catch(console.error);
